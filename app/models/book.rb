49c3fba08f136ad79b05395426afb7cddc2b07f0
Jan Roesner <jan@roesner.it>
2014-10-30 23:08:21 +0100

    Books resource

---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ActiveRecord::Base
+
+  self.include_root_in_json = true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141030220137) do
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.string   "author"
+    t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
